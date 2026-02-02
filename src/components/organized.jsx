d2db367bb6671eb6fc34e3dc520ee3782e08ecea
Eki Iman <ekiimanjordiansyah212@gmail.com>
2026-02-02 08:57:05 +0700

    adjust logo polinema

--- src/components/organized.jsx
+++ src/components/organized.jsx
@@ -9,13 +9,13 @@ function Organized() {
                 </div>
                 <div className="image col-12 col-lg-9 text-lg-start">
                     <img src="./assets/logo/LogoIYSA.WebP" className="img-fluid m-3 m-lg-5" alt=".." />
-                    <img src="./assets/logo/logoudinus.png" className="img-fluid m-3 m-lg-5" alt="" />
+                    <img src="https://res.cloudinary.com/dtik1z1qd/image/upload/v1769996852/LOGO_POLINEMA-_hbrbg1.png" className="img-fluid m-3 m-lg-5" alt="" />
                 </div>
             </div>
     </section>
         </>
     )
-  }
+}
   
   export default Organized
   
