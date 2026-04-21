a77d9b8587bff69491a84c31eabc97dfc812768b
Eki Iman <ekiimanjordiansyah212@gmail.com>
2026-04-21 16:14:50 +0700

    feat: Close MainHero component and HomeRegist registration page structure

--- src/pages/registration/homeregist.jsx
+++ src/pages/registration/homeregist.jsx
@@ -14,20 +14,27 @@ function HomeRegist() {
                 REGISTRATION FORM
               </h1>
               <h3 className="mx-auto mt-5 mb-2 text-sm md:text-lg lg:text-2xl">
-                Choose Categories Participant for Registration ISPC
-                2026
+                Choose Categories Participant for Registration ISPC 2026
               </h3>
             </div>
           </div>
           <div className="link-web mx-auto text-center">
-            <a href="/homeindo" className="btn-regist btn-action text-center me-lg-5 m-2">
+            <a href="#!" className="btn-regist btn-action text-center me-lg-5 m-2">
+              Coming Soon{" "}
+              <i className="fa-solid fa-earth-americas"></i>
+            </a>
+            {/* <a href="/homeindo" className="btn-regist btn-action text-center me-lg-5 m-2">
               Indonesia Participant{" "}
               <i className="fa-solid fa-earth-americas"></i>
-            </a>
-            <a href="/homeinter" className="btn-regist btn-action text-center me-lg-5 m-2">
-              International Participant{" "}
+            </a> */}
+            <a href="#!" className="btn-regist btn-action text-center me-lg-5 m-2">
+              Coming Soon{" "}
               <i className="fa-solid fa-earth-americas"></i>
             </a>
+            {/* <a href="/homeinter" className="btn-regist btn-action text-center me-lg-5 m-2">
+              International Participant{" "}
+              <i className="fa-solid fa-earth-americas"></i>
+            </a> */}
           </div>
         </div>
       </section>
