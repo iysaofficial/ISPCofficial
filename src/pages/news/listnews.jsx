c8c101b0e3d9958ef818282637ace09092a7d40a
IYSA <youngscientist.iysa@gmail.com>
2026-06-19 20:28:21 +0800

    up news

--- src/pages/news/listnews.jsx
+++ src/pages/news/listnews.jsx
@@ -40,6 +40,194 @@ function ListNews() {
               </a>
             </div>
             {/* NEWS 1 END */}
+            {/* NEWS 2 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20252.jpeg"
+                alt=""
+              />
+              <h5>
+                Tim Riset MAN 13 Jakarta Borong 4 Penghargaan di Ajang Internasional ISPC 2025
+              </h5>
+              <p>
+                {" "}
+                Jakarta (Humas MAN 13 Jakarta)--Tim peneliti muda dari MAN 13 Jakarta Selatan, yang beranggotakan Ghai Sani Salsabila, Raihana Naazneen Farzana, dan Zahia Uzma, berhasil mengharumkan nama sekolah dengan meraih berbagai penghargaan bergengsi dalam International Science Project Competition (ISPC) 2025.
+              </p>
+              <a
+                href="https://dki.kemenag.go.id/berita/tim-riset-man-13-jakarta-borong-4-penghargaan-di-ajang-internasional-ispc-2025-3PCfq"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 2 END */}
+            {/* NEWS 3 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20253.jpg"
+                alt=""
+              />
+              <h5>
+                Inovasi Tim Mahasiswa ITB Olah Limbah Kulit Jeruk Jadi Bioinsektisida, Raih Prestasi di ISPC 2025
+              </h5>
+              <p>
+                {" "}
+                BANDUNG, itb.ac.id - Tim mahasiswa Institut Teknologi Bandung (ITB) meraih prestasi pada ajang International Scientific Paper Competition (ISPC) 2025 yang digelar di Universitas Negeri Semarang (UNNES).
+              </p>
+              <a
+                href="https://itb.ac.id/berita/inovasi-tim-mahasiswa-itb-olah-limbah-kulit-jeruk-jadi-bioinsektisida-raih-prestasi-di-ispc-2025/62824"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 3 END */}
+            {/* NEWS 4 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20254.png"
+                alt=""
+              />
+              <h5>
+                5th International Scientific-Practical Conference, ISPC 2025: “Digital Management for a Fast-Changing World”
+              </h5>
+              <p>
+                {" "}
+                The 5th International Scientific-Practical Conference, ISPC 2025: “Digital Management for a Fast-Changing World” organized by East European University.
+              </p>
+              <a
+                href="https://eeu.edu.ge/portfolio/5thispc-2025/?lang=en"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 4 END */}
+            {/* NEWS 5 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20255.jpeg"
+                alt=""
+              />
+              <h5>
+                Sultan Naufal Haruni Mahasiswa Magister Peternakan FPP Undip. Meraih Gold Medal di Kompetisi ISPC 2025
+              </h5>
+              <p>
+                {" "}
+                Sultan Naufal Haruni, mahasiswa program magister Peternakan di Fakultas Peternakan dan Pertanian Universitas Diponegoro meraih medali emas (Gold Medal) dengan Inovasi Desa Terintegrasi.
+              </p>
+              <a
+                href="https://mit.fpp.undip.ac.id/2025/12/12/sultan-naufal-haruni-mahasiswa-magister-peternakan-fpp-undip-meraih-gold-medal-di-kompetisi-international-science-project-competition-ispc-2025-dengan-inovasi-desa-terintegrasi/"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 5 END */}
+          </div>
+          <br />
+          <h1 className="fw-bold">News 2024</h1>
+          <div className="wrapper row">
+            {/* NEWS 1 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20241.jpg"
+                alt=""
+              />
+              <h5>
+                Membanggakan, Siswi Ini Raih Medali Emas di Kompetisi Internasional ISPC 2024
+              </h5>
+              <p>
+                {" "}
+                Kompetisi sains begengsi itu diikuti oleh ratusan partisipan dari berbagai negara di seluruh dunia, baik melalui platform daring maupun luring.
+              </p>
+              <a
+                href="https://www.suara.com/lifestyle/2024/09/20/191523/membanggakan-siswi-ini-raih-medali-emas-di-kompetisi-internasional-ispc-2024"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 1 END */}
+            {/* NEWS 2 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20242.jpeg"
+                alt=""
+              />
+              <h5>
+                Angkat Topik Penelitian tentang Pecalang, Tim Riset MAN 3 Jembrana Raih Medali Perak di Kompetisi Internasional
+              </h5>
+              <p>
+                {" "}
+                Tim Riset MAN 3 Jembrana berhasil meraih medali perak dalam ajang International Science Project Competition (ISPC), Minggu (1/9/2024).
+              </p>
+              <a
+                href="https://kemenag.go.id/internasional/angkat-topik-penelitian-tentang-pecalang-tim-riset-man-3-jembrana-raih-medali-perak-di-kompetisi-internasional-DYJwa"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 2 END */}
+            {/* NEWS 3 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20243.jpeg"
+                alt=""
+              />
+              <h5>
+                SMA Trensains Sabet Medali Perak International Science Project Competition | Tebuireng Online
+              </h5>
+              <p>
+                {" "}
+                Tiga (3) Siswi SMA Trensains berhasil membawa Mendali Perak, dalam ajang lomba International Science Project Competition (ISPC) yang diselenggarakan oleh Indonesia Youth Science Assosiaton (IYSA) dan bertempat di Universitas Muhammad Malang (UMM) pada tanggal 30 Agustus 2024.
+              </p>
+              <a
+                href="https://tebuireng.online/sma-trensains-sabet-medali-perak-international-science-project-competition/"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 3 END */}
+            {/* NEWS 4 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20244.jpg"
+                alt=""
+              />
+              <h5>
+                Mahasiswa Unhas raih medali emas dalam ajang ISPC 2024
+              </h5>
+              <p>
+                {" "}
+                Tim mahasiswa Universitas Hasanuddin (Unhas) berhasil meraih medali emas dalam ajang International Science Project Competition (ISPC) 2024.
+              </p>
+              <a
+                href="https://makassar.antaranews.com/berita/557447/mahasiswa-unhas-raih-medali-emas-dalam-ajang-ispc-2024"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 4 END */}
           </div>
         </div>
       </section>
