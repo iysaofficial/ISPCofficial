377813675738763cf04f8571e5aa1a33ee7288ae
IYSA <youngscientist.iysa@gmail.com>
2026-07-28 11:09:21 +0700

    update IMAGEKIT

--- src/components/organized.jsx
+++ src/components/organized.jsx
@@ -9,7 +9,7 @@ function Organized() {
                 </div>
                 <div className="image col-12 col-lg-9 text-lg-start">
                     <img src="./assets/logo/LogoIYSA.WebP" className="img-fluid m-3 m-lg-5" alt=".." />
-                    <img src="https://res.cloudinary.com/dtik1z1qd/image/upload/v1769996852/LOGO_POLINEMA-_hbrbg1.png" className="img-fluid m-3 m-lg-5" alt="" />
+                    <img src="https://ik.imagekit.io/iysaimg/LOGO_POLINEMA-_hbrbg1.png?updatedAt=1781963634272" className="img-fluid m-3 m-lg-5" alt="" />
                 </div>
             </div>
     </section>
